be05eca1d0d044fe9822a904756d954b25b5c35e
Louis <louis_latreille@hotmail.com>
2021-10-03 18:23:05 +0000

    Refactor for cleaner structure

--- src/lib.rs
+++ src/lib.rs
@@ -6,54 +6,83 @@ use std::net::SocketAddr;
 use std::sync::Mutex;
 use inputbot::{KeybdKey::*, handle_input_events};
 
-pub struct TvCommand {
+pub struct TvCommandBinding {
     name: String,
-    command_bytes: [u8; 24]
+    command_bytes: [u8; 24],
+    key: inputbot::KeybdKey,
+    tv_commands_sender: mpsc::Sender<[u8; 24]>
 }
 
-impl TvCommand {
-    fn new(name: String, command_bytes: [u8; 24]) -> TvCommand {
-        TvCommand {
+impl TvCommandBinding {
+    fn new(name: String, command_bytes: [u8; 24], key: inputbot::KeybdKey, tv_commands_sender: mpsc::Sender<[u8; 24]>) -> TvCommandBinding {
+        TvCommandBinding {
             name,
-            command_bytes
+            command_bytes,
+            key,
+            tv_commands_sender
         }
+    }
+
+    pub fn create_command_dispatcher(&self) -> Box<dyn Fn() -> () + Send + Sync> {
+        let cloned_sender = Mutex::new(self.tv_commands_sender.clone());
+        let cloned_name = self.name.clone();
+        let cloned_bytes = self.command_bytes.clone();
+
+        let commands_dispatcher = move || {
+            println!("Sending command {}", cloned_name);
+
+            match cloned_sender.lock() {
+                Ok(sender) => {
+                    match sender.send(cloned_bytes) {
+                        Ok(_) => (),
+                        Err(_) => { println!("Error sending command {}. Receiving end likely hung up.", cloned_name) },
+                    }
+                },
+                Err(_) => { println!("Error acquiring lock to send command {}", cloned_name) }
+            }
+        };
+
+        Box::new(commands_dispatcher)
     }
 }
 
-impl Clone for TvCommand {
+impl Clone for TvCommandBinding {
     fn clone(&self) -> Self {
-        TvCommand {
+        TvCommandBinding {
             name: self.name.clone(),
-            command_bytes: self.command_bytes.clone()
+            command_bytes: self.command_bytes.clone(),
+            key: self.key.clone(),
+            tv_commands_sender: self.tv_commands_sender.clone()
         }
     }
 }
 
-pub struct TvCommandsManager {
-    commands: [TvCommand; 14],
-    sender: mpsc::Sender<[u8; 24]>,
-}
+pub struct TvCommandsManager {}
 
 impl TvCommandsManager {
     pub fn new(tv_address: SocketAddr) -> TvCommandsManager {
-        let commands = [
-            TvCommand::new(String::from("powerOff"), *b"*SCPOWR0000000000000000\n"),
-            TvCommand::new(String::from("powerOn"), *b"*SCPOWR0000000000000001\n"),
-            TvCommand::new(String::from("home"), *b"*SCIRCC0000000000000006\n"),
-            TvCommand::new(String::from("netflix"), *b"*SCIRCC0000000000000056\n"),
-            TvCommand::new(String::from("up"), *b"*SCIRCC0000000000000009\n"),
-            TvCommand::new(String::from("down"), *b"*SCIRCC0000000000000010\n"),
-            TvCommand::new(String::from("right"), *b"*SCIRCC0000000000000011\n"),
-            TvCommand::new(String::from("left"), *b"*SCIRCC0000000000000012\n"),
-            TvCommand::new(String::from("enter"), *b"*SCIRCC0000000000000013\n"),
-            TvCommand::new(String::from("return"), *b"*SCIRCC0000000000000008\n"),
-            TvCommand::new(String::from("hdmi1"), *b"*SCINPT0000000100000001\n"),
-            TvCommand::new(String::from("hdmi2"), *b"*SCINPT0000000100000002\n"),
-            TvCommand::new(String::from("hdmi3"), *b"*SCINPT0000000100000003\n"),
-            TvCommand::new(String::from("hdmi4"), *b"*SCINPT0000000100000004\n"),
-        ];
+        let (tv_commands_sender, tv_commands_receiver): (mpsc::Sender<[u8; 24]>, mpsc::Receiver<[u8; 24]>) = mpsc::channel();
 
-        let (sender, receiver): (mpsc::Sender<[u8; 24]>, mpsc::Receiver<[u8; 24]>) = mpsc::channel();
+        let key_bindings = [
+            TvCommandBinding::new(String::from("powerOff"), *b"*SCPOWR0000000000000000\n", F1Key, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("powerOn"), *b"*SCPOWR0000000000000001\n", F2Key, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("home"), *b"*SCIRCC0000000000000006\n", HKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("netflix"), *b"*SCIRCC0000000000000056\n", NKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("up"), *b"*SCIRCC0000000000000009\n", UpKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("up"), *b"*SCIRCC0000000000000009\n", WKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("down"), *b"*SCIRCC0000000000000010\n", DownKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("down"), *b"*SCIRCC0000000000000010\n", SKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("right"), *b"*SCIRCC0000000000000011\n", DKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("right"), *b"*SCIRCC0000000000000011\n", RightKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("left"), *b"*SCIRCC0000000000000012\n", AKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("left"), *b"*SCIRCC0000000000000012\n", LeftKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("enter"), *b"*SCIRCC0000000000000013\n", EnterKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("return"), *b"*SCIRCC0000000000000008\n", BackspaceKey, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("hdmi1"), *b"*SCINPT0000000100000001\n", Numrow1Key, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("hdmi2"), *b"*SCINPT0000000100000002\n", Numrow2Key, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("hdmi3"), *b"*SCINPT0000000100000003\n", Numrow3Key, tv_commands_sender.clone()),
+            TvCommandBinding::new(String::from("hdmi4"), *b"*SCINPT0000000100000004\n", Numrow4Key, tv_commands_sender.clone()),
+        ];
 
         let mut stream = match TcpStream::connect(tv_address) {
             Ok(stream) => stream,
@@ -61,10 +90,10 @@ impl TvCommandsManager {
                 eprintln!("Error when connecting to TV. Is the TV still turned on and connected to the network? Error: {}", error);
                 panic!()
             }
-        };
+        };         
 
         thread::spawn(move || loop {
-            let tv_command_bytes = match receiver.recv() {
+            let tv_command_bytes = match tv_commands_receiver.recv() {
                 Ok(bytes) => bytes,
                 Err(_) => {
                     panic!("Error while listening for incoming commands. It is likely there are no more command dispatchers.")
@@ -80,58 +109,29 @@ impl TvCommandsManager {
             };
         });
 
-        TvCommandsManager {
-            commands,
-            sender
-        }
-    }
+        let (activation_sender, activation_receiver): (mpsc::Sender<u8>, mpsc::Receiver<u8>) = mpsc::channel();   
 
-    pub fn create_commands_dispatcher(&self) -> Box<dyn Fn(&str) -> () + Send + Sync> {
-        let cloned_sender = Mutex::new(self.sender.clone());
-        let cloned_commands = self.commands.to_vec();
+        let activation_sender = Mutex::new(activation_sender);
 
-        let commands_dispatcher = move |tv_command_str: &str| {
-            println!("Sending command {}", tv_command_str);
-
-            let tv_command_bytes = match cloned_commands.iter().find(|tv_command| {
-                tv_command.name == tv_command_str
-            }) {
-                Some(tv_command) => tv_command.command_bytes,
-                None => {
-                    println!("Unknown command");
-                    return ()
-                }
-            };
-
-            match cloned_sender.lock() {
+        ScrollLockKey.block_bind(move || {
+            match activation_sender.lock() {
                 Ok(sender) => {
-                    match sender.send(tv_command_bytes) {
+                    match sender.send(1) {
                         Ok(_) => (),
-                        Err(_) => { println!("Error sending command {}. Receiving end likely hung up.", tv_command_str) },
+                        Err(_) => { println!("Error sending key bindings activation command. Receiving end likely hung up.") },
                     }
                 },
-                Err(_) => { println!("Error acquiring lock to send command {}", tv_command_str) }
+                Err(_) => { println!("Error acquiring lock to send key bindings activation command") }
             }
-        };
+        });
 
-        Box::new(commands_dispatcher)
-    }
-}
-
-pub struct KeyboardInputManager {
-    sender: Mutex<mpsc::Sender<u8>>
-}
-
-impl KeyboardInputManager {
-    pub fn new() -> KeyboardInputManager {
-        let tv_commands_manager = TvCommandsManager::new(SocketAddr::from(([192, 168, 10, 106], 20060)));
-        let (sender, receiver): (mpsc::Sender<u8>, mpsc::Receiver<u8>) = mpsc::channel();
+        let cloned_key_bindings = key_bindings.to_vec();
 
         thread::spawn(move || {
             let mut key_binds_allocated = false;
 
             loop {
-                match receiver.recv() {
+                match activation_receiver.recv() {
                     Ok(_) => (),
                     Err(_) => {
                         println!("Keyboard Input Manager has disconnected. Exiting...");
@@ -144,84 +144,21 @@ impl KeyboardInputManager {
                 println!("Listening is {}", key_binds_allocated);
                 
                 if key_binds_allocated {
-                    KeyboardInputManager::allocate_bindings(&tv_commands_manager);
+                    for key_binding in cloned_key_bindings.iter() {
+                        key_binding.key.block_bind(key_binding.create_command_dispatcher());
+                    }
                 } else {
-                    KeyboardInputManager::deallocate_bindings()
+                    for key_binding in cloned_key_bindings.iter() {
+                        key_binding.key.unbind();
+                    }
                 }
             }
         });
 
-        KeyboardInputManager {
-            sender: Mutex::new(sender)
-        }
+        TvCommandsManager {}
     }
 
-    pub fn toggle_listening(&self) {
-        match self.sender.lock() {
-            Ok(sender) => {
-                match sender.send(1) {
-                    Ok(_) => (),
-                    Err(_) => { println!("Error sending key bindings activation command. Receiving end likely hung up.") },
-                }
-            },
-            Err(_) => { println!("Error acquiring lock to send key bindings activation command") }
-        }
-    }
-
-    pub fn start(keyboard_input_manager: KeyboardInputManager) {
-        ScrollLockKey.block_bind(move || {
-            keyboard_input_manager.toggle_listening();
-        });
-
+    pub fn start(&self) {
         handle_input_events();
-    }
-
-    fn allocate_bindings(tv_commands_manager: &TvCommandsManager) {
-        WKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("up"), tv_commands_manager));
-        UpKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("up"), tv_commands_manager));
-        SKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("down"), tv_commands_manager));
-        DownKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("down"), tv_commands_manager));
-        AKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("left"), tv_commands_manager));
-        LeftKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("left"), tv_commands_manager));
-        DKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("right"), tv_commands_manager));
-        RightKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("right"), tv_commands_manager));
-        NKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("netflix"), tv_commands_manager));
-        HKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("home"), tv_commands_manager));
-        F1Key.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("powerOff"), tv_commands_manager));
-        F2Key.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("powerOn"), tv_commands_manager));
-        EnterKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("enter"), tv_commands_manager));
-        BackspaceKey.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("return"), tv_commands_manager));
-        Numrow1Key.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("hdmi1"), tv_commands_manager));
-        Numrow2Key.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("hdmi2"), tv_commands_manager));
-        Numrow3Key.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("hdmi3"), tv_commands_manager));
-        Numrow4Key.block_bind(KeyboardInputManager::create_specialized_dispatcher(String::from("hdmi4"), tv_commands_manager));
-    }
-
-    fn deallocate_bindings() {
-        WKey.unbind();
-        UpKey.unbind();
-        SKey.unbind();
-        DownKey.unbind();
-        AKey.unbind();
-        LeftKey.unbind();
-        DKey.unbind();
-        RightKey.unbind();
-        NKey.unbind();
-        HKey.unbind();
-        F1Key.unbind();
-        F2Key.unbind();
-        EnterKey.unbind();
-        BackspaceKey.unbind();
-        Numrow1Key.unbind();
-        Numrow2Key.unbind();
-        Numrow3Key.unbind();
-        Numrow4Key.unbind();
-    }
-
-    fn create_specialized_dispatcher(command: String, tv_command_dispatcher: &TvCommandsManager) -> Box<dyn Fn() -> () + Send + Sync> {
-        let dispatcher = tv_command_dispatcher.create_commands_dispatcher();
-        Box::new(move || {
-            dispatcher(&command[..]);
-        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-use sony_bravia_control::KeyboardInputManager;
+use sony_bravia_control::TvCommandsManager;
+use std::net::SocketAddr;
+use configparser::ini::Ini;
 
 fn main() {
-    let keyboard_input = KeyboardInputManager::new();
-    KeyboardInputManager::start(keyboard_input);
+    let mut config = Ini::new();
+
+    let ini_filename = "sony_bravia_control.ini";
+    match config.load(ini_filename) {
+        Ok(_) => (),
+        Err(_) => eprintln!("Couldn't load the configuration from the {} file.", ini_filename)
+    }
+
+    let tv_address = config.get("default", "tv_address").expect("No tv_address configuration found in the ini file.");
+
+    let socket_addr: SocketAddr = tv_address.parse().expect("Misspelled tv address. It must be a IPv4 addresse followed by a port, such as 192.168.1.1:20060");
+    let tv_commands_manager = TvCommandsManager::new(socket_addr);
+    tv_commands_manager.start();
 }
